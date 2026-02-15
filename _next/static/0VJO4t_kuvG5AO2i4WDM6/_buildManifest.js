e269f52bffd3edc27a96e68a965a26613692cb10
chinaskii89 <107044923+dannniluk@users.noreply.github.com>
2026-02-15 22:00:54 +0300

    Deploy GitHub Pages: d15fc82

--- _next/static/0VJO4t_kuvG5AO2i4WDM6/_buildManifest.js
+++ _next/static/0VJO4t_kuvG5AO2i4WDM6/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/creatorshub//_next/:path+",
-        "destination": "/creatorshub/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
